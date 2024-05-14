8596f4ce01d2708a4b854750844f4a1a4124b678
Victor Paiva <129319485+victorpaivas@users.noreply.github.com>
2024-05-14 16:25:30 -0300

    Add files and folder in .gitignore

--- src/functional_interface/example/SupplierExample.java
+++ src/functional_interface/example/SupplierExample.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
  */
 public class SupplierExample {
     public static void main(String[] args) {
-        
+
         // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
         Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
 
